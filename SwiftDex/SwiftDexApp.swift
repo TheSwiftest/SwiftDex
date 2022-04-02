a1e396b289e0de2d989e293e230ed4b3e557db34
Brian <brian.william.corbin@gmail.com>
2022-04-02 14:14:26 -0400

    Initial Commit

---
+++ SwiftDex/SwiftDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDexApp.swift
+//  SwiftDex
+//
+//  Created by Brian Corbin on 4/2/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
